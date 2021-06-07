ad5bef5c5977e5239caab2ca908fc69b228aa579
Arthur Pimentel Cordeiro <55156476+ARTHURPC03@users.noreply.github.com>
2021-06-07 08:21:13 -0300

    🏗feat: Listing Repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,12 +3,6 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([])
 
@@ -22,11 +16,10 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
-      <ul> 
-        <RepositoryItem repository={repository} /> 
-        <RepositoryItem repository={repository} /> 
-        <RepositoryItem repository={repository} /> 
-        <RepositoryItem repository={repository} /> 
+      <ul>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} /> 
+        })}
       </ul>
     </section>
   )
